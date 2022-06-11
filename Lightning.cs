26da4d42958158e5f93e350c172d0ab8432feb39
BaileyFirman <bailey.firman@nintex.com>
2022-06-11 19:43:58 +1200

    + Call scanner within main + Add test file

--- Lightning.cs
+++ Lightning.cs
@@ -1,10 +1,21 @@
-﻿using Lightning.Scanner;
+﻿using Lightning.Scanning;
 using Lightning.Utils;
 
-Benchmark.Run(
+var file = Benchmark.Run(
+    "File Retrived In:",
+    () => args.Length == 1 ? File.ReadAllText(args[0]) : string.Empty
+);
+
+var tokens = Benchmark.Run(
     "Scanned In:",
     () =>
     {
         var scanner = new LightningScanner();
+        return scanner.ScanTokens(file);
     }
 );
+
+foreach (var token in tokens)
+{
+    Console.WriteLine(token);
+}
